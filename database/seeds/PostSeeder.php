3e194234a16e4fc92ec4703cd2f088201fd06436
noemipintaldi <noemipintaldi@gmail.com>
2022-06-14 18:10:25 +0200

    relazione/migration/seeding completate

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\Category;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -14,13 +16,17 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        //$array = [1,2,3,4]
+        $category_ids = Category::pluck('id')->toArray();
+
         for ($i = 0; $i < 10; $i++) { 
             $post = new Post();
+            $post -> category_id = Arr::random($category_ids);
             $post -> title = $faker->text(10);
             $post -> content = $faker->paragraph(2);
             $post -> image = $faker->imageUrl(250, 250);
             $post -> slug = Str::slug($post -> title, '-');
-            $post -> save();
+            @$post -> save();
         }
     }
 }
